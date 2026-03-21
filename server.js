b614fcbcac5f61e3c1749afa8b677aff765dba12
nkrish56377-creator <nkrish56377@gmail.com>
2026-03-21 23:17:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -66,5 +66,6 @@ app.post("/reset", (req, res) => {
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));;
 
-app.listen(3000, () => console.log("Server running"));
